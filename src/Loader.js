a1d15853cb64e26639adae88876837afc24d6b09
MarianaMarkevych <markevycha92@gmail.com>
2022-06-15 11:24:35 +0300

    initial commit

---
+++ src/Loader.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Loader = () => {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', margin: '.5rem' }}>
+    <div className='lds-dual-ring' />
+  </div>
+  )
+}
+
+export default Loader;
+
+
+
